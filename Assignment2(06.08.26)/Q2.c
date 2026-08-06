9b99ed21afc9172f31e062124da0c68c25b349f2
Anish Sinha <sinha.anish008@gmail.com>
2026-08-06 15:25:26 +0530

    Add comments for marks input and sorting functionality
    Add initial comments for Q2.c regarding functionality.

---
+++ Assignment2(06.08.26)/Q2.c
@@ -0,0 +1,3 @@
+/*WACP to take input marks of ten student randomly in an array sort the array using management and show the highest and the lowest marks
+input size of array = 10
+[80,73,59,92,86,47,92,58,91,48]*/
